5a71ca31b00511e60515f2aaf6db8526d8752cb9
Aaron ALAYO <charlotteflanding@Min-MacBook.local>
2020-02-06 09:51:43 +0100

    added the first exercises

---
+++ 1/1._Intro/Intro_Exercises/1.Basics.js
@@ -0,0 +1,14 @@
+console.log("hi");
+
+var firstName = "Anders"
+var lastName = "Latif"
+
+console.log("My first name is ", firstName, "and my last name is", lastName);
+
+
+
+
+var year = "2017";
+var number = 1
+
+console.log()
