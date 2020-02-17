a8f4066140a793cc4316e3d5e903edc95415f9cb
Aaron ALAYO <aaro0186@stud.kea.dk>
2020-02-17 13:32:57 +0100

    week7

--- 1/2._First_API/app.js
+++ 1/2._First_API/app.js
@@ -1,11 +1,26 @@
 // const express = require("express");
 // const app = express();
+// var liveServer = require("live-server");
+ 
+// var params = {
+//     port: 8080, // Set the server port. Defaults to 8080.
+//     host: "localhost", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+//     root: "/", // Set root directory that's being served. Defaults to cwd.
+//     open: true, // When false, it won't load your browser by default.
+//     ignore: 'scss,my/templates', // comma-separated string for paths to ignore
+//     file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+//     wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+//     mount: [['/components', './node_modules']], // Mount a directory to a route.
+//     logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+//     middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+// };
+// liveServer.start(params);
 
 const app = require("express")();
 
 app.get("/", (req, res) => {
     res.send({
-        message : "Hello"
+        message : "Hi"
     });
 
 });
@@ -40,7 +55,12 @@ app.get("/functionality", (req, res) => {
         day : d.getDate(),
         month : d.getUTCMonth(),
         year : d.getUTCFullYear(),
-        time : d.getMinutes()
+        minutes : d.getMinutes(),
+        hours : d.getHours(),
+        seconds : d.getSeconds(),
+        mili : d.getMilliseconds(),
+      message : "hello"
+        
     }
     
     res.send(functionality);
