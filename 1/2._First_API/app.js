531f117b506738d9035d36a33165504e87e615ff
aaronalayo <aaro0186@stud.kea.dk>
2020-02-20 17:51:15 +0000

    Budo and jQuery

--- 1/2._First_API/app.js
+++ 1/2._First_API/app.js
@@ -1,5 +1,7 @@
 const app = require("express")();
 
+const request = require('request');
+
 app.get("/", (req, res) => {
     res.send({
         message : "Hi"
@@ -31,24 +33,72 @@ app.get("/aboutThisWebsite", (req, res) =>{
     //no code here won't run
 });
 
-app.get("/functionality", (req, res) => {
-    var d = new Date();
-    const functionality = {
-        day : d.getDate(),
-        month : d.getUTCMonth(),
-        year : d.getUTCFullYear(),
-        minutes : d.getMinutes(),
-        hours : d.getHours(),
-        seconds : d.getSeconds(),
-        mili : d.getMilliseconds(),
-      message : "he"
-        
-    }
-    
-    res.send(functionality);
-})
 
-port = 5000;
+app.get("/time", (req, res) => {
+    const date = new Date();
+    
+    res.send( date.toLocaleString('en-EU', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'}));
+    });
+
+app.get("/testroute", (req,res) => {
+
+    if(true){
+
+        return res.send({message : "inside the if"});
+    }
+    return res.send({message: "Outside the if"});
+});
+
+app.get("/user/:id", (req, res) => {
+
+    console.log(req.params);
+    return res.send({ id: req.params.id});
+});
+
+app.get("/search", (req, res) => {
+
+    console.log(req.query)
+    return res.send(req.query);
+
+});
+
+
+app.get("/google", (req, res) => {
+
+    request('http://www.google.com', (error, response, body) => {
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    return res.send(body);
+});
+   
+});
+
+app.get("/documentation", (req, res) => {
+
+   // console.log(__dirname);
+   //   return res.redirect("/documentationtwo")
+    return res.sendFile(__dirname + "/public/documentation.html");
+});
+
+app.get("/documentationtwo", (req, res) => {
+
+    console.log(__dirname);
+    return res.sendFile(__dirname + "/public/documentationtwo.html");
+});
+
+
+
+
+
+
+
+
+port = 3000;
 app.listen(port, (error) => {
     if(error){
         console.log(error)
