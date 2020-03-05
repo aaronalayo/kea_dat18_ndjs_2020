abf4f3279e121d040bcc2fb769b5b294df8ad1cb
aaronalayo <aaro0186@stud.kea.dk>
2020-03-05 16:12:06 +0100

    device and video api

--- 1/4._Devices/app.js
+++ 1/4._Devices/app.js
@@ -1,12 +1,19 @@
-const app = require("express")()//instantiate;
+const express = require("express")//instantiate;
 
-const request = require('request');
+const app = express();
+
+app.use(express.urlencoded({extended:false})); //to get response fromm
+
+app.use(express.json());//to sumit form
 
 let devices = [
     {id: 0, type:'mobile'}, 
     {id: 1, type: 'ipad'},
     {id: 2, type: 'pc'}
 ];
+
+let nextDeviceId = 3;
+
 
 app.get("/", (req, res) => {
     res.send("this is node API")
@@ -17,19 +24,75 @@ app.get("/", (req, res) => {
 app.get("/devices", (req, res) => {
 
    
-    return res.send({device: devices});
+    return res.send({response: devices});
 });
 
 app.get("/devices/:id", (req, res) => {
     const device = device.find(device => device.id === Number(req.params.id));
-    return res.send({device: device})
+    return res.send({response: device});
 
 });
 
-app.post("/test", (req, res) => {
-    console.log("test test")
-    res.send({});
-})
+
+
+app.post("/devices",(req, res) => {
+
+    let newDevice = req.body;
+    if(!newDevice.type){
+
+        return res.status(400).send({response: "Missing the device type"});
+    }
+    // const maxId = devices.reduce((previous, current) => {
+        
+    //     if (current.id > previous.id){
+    //         return current.id;
+
+    //     }else {
+
+    //         return previous.id
+    //     }
+    // });
+
+    
+    newDevice.id = nextDeviceId++;
+    devices.push(newDevice)
+   
+    console.log(devices)
+    return res.send({response: newDevice });
+});
+
+
+
+app.put('/devices/:id', function (req, res) {
+    
+    const foundIndex = devices.findIndex(device => device.id === Number(req.params.id));
+    delete req.body.id;
+    const newDevice = {...devices[foundIndex],...req.body}; //spread to new object
+
+    devices[foundIndex] = newDevice;
+
+    console.log(devices)
+
+    return res.send({response: newDevice})
+  });
+
+
+
+
+
+
+  app.delete("/devices/:id", (req, res) => {
+
+    devices = devices.filter(device => device.id != req.params.id)
+    
+    console.log(devices)
+    return res.send({response : devices})
+
+});
+
+
+
+
 
 
 const server = app.listen(3000, (error) => {
---
+++ 1/5._Keatube/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({extended:false})); //to get response fromm
+
+// parse application/json
+app.use(express.json());//to sumit form
+
+
+console.log("This is from the enviroment variables",);
+
+app.get("/video/:videoid", (req, res) => {
+
+    return res.sendFile(__dirname + "/public/video.html");
+});
+
+app.use(express.static('public'));
+app.use(express.static('video'));
+
+
+const port = process.env.PORT ? process.env.PORT : 3000;
+
+const server = app.listen(port, (error) => {
+    if(error){
+        console.log("error running the server")
+    } 
+        console.log("App listening on port: ", server.address().port)
+    
+});
